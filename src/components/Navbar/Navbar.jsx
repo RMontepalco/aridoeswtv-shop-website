6509546c7e06a0d20534eb7de3da5a4147a68571
RMontepalco <montepalco.ross@gmail.com>
2024-09-25 15:41:17 -0700

    Created Products Dropdown Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import chevronCompactDown from '/chevron-compact-down.svg'
 import './Navbar.css'
 
 export default function Navbar() {
@@ -6,7 +7,30 @@ export default function Navbar() {
     <nav className="navbar">
       <h1>aridoeswtv</h1>
       <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
+
+      {/*
+        TO DO: Create a Products dropdown menu with the following criteria:
+        Clicking Products will display all product categories in one page
+        Hovering over Products will display a dropdown menu containing
+          Keychains, Hairclips, Jewlery, misc.
+        Clicking on a dropdown menu item will only display that category
+          of products
+      */}
+      <div className="products-dropdown-menu">
+        <Link className="products-button" to="/products">
+          Products <div className="chevron"></div>
+        </Link>
+
+        <div className="products-dropdown-content">
+          <Link to="/products">Keychains</Link>
+          <Link to="/products">Hairclips</Link>
+          <Link to="/products">Jewlery</Link>
+          <Link to="/products">Prints</Link>
+          <Link to="/products">Stickers</Link>
+          <Link to="/products">Misc.</Link>
+        </div>
+      </div>
+
       <Link to="/contact">Contact</Link>
     </nav>
   )
