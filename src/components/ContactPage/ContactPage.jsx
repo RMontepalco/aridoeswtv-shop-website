854dae2d22cb12a0f6fbec930f363c857adfe1e1
RMontepalco <montepalco.ross@gmail.com>
2024-12-16 14:25:15 -0800

    Added header and order information to ContactPage

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -3,7 +3,44 @@ import './ContactPage.css'
 export default function ContactPage() {
   return (
     <div className="contact">
-      <h1>Contact Page</h1>
+      <h1>ʚ Payment Methods ɞ</h1>
+      <div style={{display: "flex", columnGap: "30px", width: "100%"}}>
+        <div className="order-method">
+          <h2>Local Orders</h2>
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            Rutrum dolor facilisis vulputate parturient in lacus cras at? 
+            Cubilia per mi donec ridiculus morbi platea urna amet. 
+            Eleifend nunc arcu euismod sit porta ligula ligula nascetur. 
+            Torquent class magnis sem commodo luctus eu cubilia mauris.
+          </p>
+        </div>
+        <div className="order-method">
+          <h2>International Orders</h2>
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            Rutrum dolor facilisis vulputate parturient in lacus cras at? 
+            Cubilia per mi donec ridiculus morbi platea urna amet. 
+            Eleifend nunc arcu euismod sit porta ligula ligula nascetur. 
+            Torquent class magnis sem commodo luctus eu cubilia mauris.
+          </p>
+        </div>
+      </div>
+      <div className="order-method">
+        <h2>How to Order!</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit.
+          Rutrum dolor facilisis vulputate parturient in lacus cras at? 
+          Cubilia per mi donec ridiculus morbi platea urna amet. 
+          Eleifend nunc arcu euismod sit porta ligula ligula nascetur. 
+          Torquent class magnis sem commodo luctus eu cubilia mauris. 
+          Semper praesent consectetur facilisi lorem mi ipsum commodo. 
+          Blandit vestibulum mollis donec finibus ornare curae. 
+          Convallis at adipiscing euismod gravida bibendum class tincidunt aenean? 
+          Feugiat taciti quam eleifend porta pharetra. 
+          Senectus odio nullam dapibus mi orci eleifend feugiat.
+        </p>
+      </div>
     </div>
   )
 }
